6ff21107f13a1cbbe4e5171c87fcf7c56b7ed3b3
Raymw1 <rayan.rmw@gmail.com>
2021-10-01 15:16:42 -0300

    ReactNative - Listing products done

---
+++ 05-react-native/hunt/src/config/HeaderStyle.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'JSHunt',
+  headerStyle: {
+    backgroundColor: '#DA552F',
+  },
+  headerTintColor: '#FFF',
+};
--- 05-react-native/hunt/src/pages/main.js
+++ 05-react-native/hunt/src/pages/main.js
@@ -1,17 +1,96 @@
 import React, {Component} from 'react';
+import api from '../services/api';
 
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import HeaderStyle from '../config/HeaderStyle';
 
 export default class Main extends Component {
-  static navigationOptions = {
-    title: 'JSHunt',
+  static navigationOptions = HeaderStyle;
+
+  state = {
+    products: [],
+    page: 1,
   };
 
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    try {
+      const response = await api.get('/products');
+      const {docs} = response.data;
+      this.setState({products: docs});
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  renderItem = ({item}) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
+      <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+        <Text style={styles.productButtonText}>Access</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   render() {
+    const {products} = this.state;
     return (
-      <View>
-        <Text>Página Main</Text>
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={products}
+          keyExtractor={product => product._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Use full screen
+    backgroundColor: '#FAFAFA',
+  },
+  list: {
+    padding: 20,
+  },
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24,
+  },
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  productButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#DA552F',
+  },
+});
